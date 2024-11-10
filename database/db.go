236471dfef68606540a7db5e959d9e42bd1b9e70
Arya Sukma Wijaya <aryasukma2@gmail.com>
2024-11-10 19:50:41 +0700

    add environment

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,9 +11,22 @@ import (
 var DB *gorm.DB
 
 func DBInit() {
-	const MYSQL = "root:@tcp(127.0.0.1:3306)/go_fiber_gorm?charset=utf8mb4&parseTime=True&loc=Local"
-
 	var err error
+
+	viper.SetConfigFile(".env.toml")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	db_host := viper.GetString("database.DB_HOST")
+	db_port := viper.GetInt("database.DB_PORT")
+	db_name := viper.GetString("database.DB_DATABASE")
+	db_user := viper.GetString("database.DB_USERNAME")
+	db_pass := viper.GetString("database.DB_PASSWORD")
+
+	MYSQL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_user, db_pass, db_host, db_port, db_name)
 
 	dsn := MYSQL
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
